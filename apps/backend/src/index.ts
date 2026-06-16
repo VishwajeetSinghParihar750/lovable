50b299f96eb444eb93473234f514b07b0c9ca3f7
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-06-16 20:08:32 +0000

    prisma

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
 app.use(express.json());
 
-//
+app.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+});
+app.post("/signin", (req, res) => {
+  //
+});
+
+app.post("/project", (req, res) => {
+  //
+});
+app.post("/project/:id", (req, res) => {
+  //
+});
 
 app.listen(3000);
