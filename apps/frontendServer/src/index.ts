6b444fc1558f6f34d4f16d4beb9bfdb580152840
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-06-17 15:04:02 +0000

    s3 working

--- apps/frontendServer/src/index.ts
+++ apps/frontendServer/src/index.ts
@@ -1,5 +1,18 @@
-function main() {
-  //
+import "dotenv/config";
+import {
+  GetObjectCommand,
+  HeadBucketCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({ region: process.env.AWS_REGION! });
+
+async function main() {
+  const res2 = await s3.send(
+    new GetObjectCommand({ Bucket: process.env.AWS_BUCKET_NAME, Key: "0" }),
+  );
+  console.log(await res2.Body?.transformToString());
 }
 
 main();
