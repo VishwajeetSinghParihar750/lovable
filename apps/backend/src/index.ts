04288e5d498e1da1929ef9d18d09d497fa2066a6
sahil <svishwajeet750@gmail.com>
2026-07-17 02:24:51 +0530

    deployment, cluster template creation

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,7 +5,7 @@ import config from "./k8s/podConfig.json";
 
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
-const appsApi = kc.makeApiClient(k8s.AppsV1Api);
+const k8sClient = k8s.KubernetesObjectApi.makeApiClient(kc);
 
 const [deploymentYaml, serviceYaml] = deploymentService;
 // this should be brought in from redis thing on every pod creation
@@ -14,24 +14,40 @@ const { USER_APP_IMAGE, USER_APP_PORT, AI_APP_IMAGE, AI_APP_PORT } = config;
 
 const startPod = async () => {
   //
-
+  console.dir(kc.getCurrentCluster(), { depth: null });
   const workerId = crypto.randomUUID();
   let userAppUrl = "";
 
-  console.log(deploymentYaml);
-  const deployment = deploymentYaml
-    .replaceAll("${WORKER_NAME}", workerId)
-    .replaceAll("${USER_APP_IMAGE}", USER_APP_IMAGE)
-    .replaceAll("${USER_APP_PORT}", USER_APP_PORT)
-    .replaceAll("${AI_APP_IMAGE}", AI_APP_IMAGE)
-    .replaceAll("${AI_APP_PORT}", AI_APP_PORT);
+  const deployment = structuredClone(deploymentYaml);
+  deployment.metadata.name = workerId;
+  deployment.metadata.labels.app = workerId;
 
-  const service = serviceYaml
-    .replaceAll("${WORKER_NAME}", workerId)
-    .replaceAll("${USER_APP_PORT}", USER_APP_PORT)
-    .replaceAll("${AI_APP_PORT}", AI_APP_PORT);
+  deployment.spec.selector.matchLabels.app = workerId;
+  deployment.spec.template.metadata.labels.app = workerId;
+
+  deployment.spec.template.spec.containers[0].image = USER_APP_IMAGE;
+  deployment.spec.template.spec.containers[0].ports[0].containerPort =
+    Number(USER_APP_PORT);
+
+  deployment.spec.template.spec.containers[1].image = AI_APP_IMAGE;
+  deployment.spec.template.spec.containers[1].ports[0].containerPort =
+    Number(AI_APP_PORT);
+
+  const service = structuredClone(serviceYaml);
+
+  service.metadata.name = workerId;
+
+  service.spec.selector.app = workerId;
+
+  service.spec.ports[0].port = 3000;
+  service.spec.ports[0].targetPort = Number(USER_APP_PORT);
+
+  service.spec.ports[1].port = 80;
+  service.spec.ports[1].targetPort = Number(AI_APP_PORT);
 
   console.log(deployment, service);
+
+  await Promise.all([k8sClient.create(deployment), k8sClient.create(service)]);
 
   return { workerId, userAppUrl };
 };
